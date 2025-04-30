6e9c6de4fd9e69bfcee83b5af3705e49ea4b879f
alejandroEsc <jaescobar.cell@gmail.com>
2025-04-29 19:31:39 -0700

    changing prompt

--- sample.py
+++ sample.py
@@ -12,7 +12,7 @@ import sentencepiece as spm
 # -----------------------------------------------------------------------------
 init_from = 'resume' # either 'resume' (from an out_dir) or a gpt2 variant (e.g. 'gpt2-xl')
 out_dir = 'out' # ignored if init_from is not 'resume'
-start = "\n" # or "<|endoftext|>" or etc. Can also specify a file, use as: "FILE:prompt.txt"
+start = "What is the metric of a rotating blackhole" # or "<|endoftext|>" or etc. Can also specify a file, use as: "FILE:prompt.txt"
 num_samples = 10 # number of samples to draw
 max_new_tokens = 500 # number of tokens generated in each sample
 temperature = 0.8 # 1.0 = no change, < 1.0 = less random, > 1.0 = more random, in predictions
@@ -90,7 +90,7 @@ with torch.no_grad():
     with ctx:
         for k in range(num_samples):
             # Decode token IDs back to text
-            out = model.generate(x, max_new_tokens=500)
+            out = model.generate(x, max_new_tokens=500, temperature=temperature, top_k=top_k)
             out_text = sp.decode(out[0].tolist())
             print(out_text)
             print('---------------')
